86b26180d225958554ebcae892cfab1359561891
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 15:13:40 +0530

    Changes for response and handling of function result

--- controllers/workflow.controller.js
+++ controllers/workflow.controller.js
@@ -63,21 +63,48 @@ const createWorkflowCon = async (req, res) => {
 // Function to get workflow
 const getWorkflowCon = async (req, res) => {
     try {
+        // Initialize variables
+        const response = {
+            status: 'fail',
+            message: 'Failed to retrieve workflow',
+            data: {},
+            errors: []
+        };
+        let errors          = [];
+        let statusCode      = 400;
+
+        // Retrieve the data from the request
         const { workflowId } = req.params;
 
         // Call the getWorkflow service
         const workflow = await getWorkflow(workflowId);
 
-        // Define the response object
-        const response = {
-            status: 200,
-            message: "Workflow retrieved successfully",
-            data: workflow,
-            errors: []
-        };
+        // Check if the workflow was retrieved successfully
+        if (workflow.status != 'success') {
+            errors = [
+                ...errors,
+                ...workflow.errors,
+            ];
+
+            // Update the response object
+            response = {
+                ...response, // Spread the existing response object
+                errors: errors
+            };
+        } else {
+            // Update the response object
+            response = {
+                ...response, // Spread the existing response object
+                status: 'success',
+                message: 'Workflow retrieved successfully',
+                data: {
+                    workflow: workflow.data.workflow
+                },
+            };
+        }
 
         // Send the response
-        sendResponse(res, 200, response);
+        sendResponse(res, statusCode, response);
     } catch (error) {
         // Define the response object
         const response = {

5c5c2c8161de40c39bb2cbcbb4d857fc72afeb6e
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 14:57:27 +0530

    Implemented wait function to sleep

---
+++ utils/js.utils.js
@@ -0,0 +1,33 @@
+// Function for waiting
+const wait = async (ms) => {
+    try {
+        // initialize variables
+        const response = {
+            status: 'success',
+            message: 'Wait executed successfully',
+            data: {},
+            errors: []
+        };
+
+        // Sleep for the specified amount of time
+        await new Promise(resolve => setTimeout(resolve, ms));
+
+        // Return the response
+        return response;
+    } catch (error) {
+        // Define the response object
+        const response = {
+            status: "fail",
+            message: "Exception occurred",
+            data: {},
+            errors: []
+        };
+
+        // return the response
+        return response;
+    }
+};
+
+export {
+    wait,
+}

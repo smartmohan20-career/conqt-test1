f2574083846d2d3b569ae917fa534bd16a190dc9
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 15:45:43 +0530

    Changes to use env variables from config file

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 import pg from "pg";
+import config from "./env.js";
 
 // Import the pg module
 const { Pool } = pg;
 
+// Retrieve required details from config
+const { DB_USER, DB_HOST, DB_NAME, DB_PASS, DB_PORT } = config;
+
+// Create a new pool
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "conqt-test1",
-    password: "Hkrnlp8#",
-    port: 5432,
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_NAME,
+    password: DB_PASS,
+    port: DB_PORT,
 });
 
 export default pool;

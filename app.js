3a9399fa098eb3ecdb8d8c7c5ea2dd30f7220bd4
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 14:59:28 +0530

    Implemented express server with initializations and loading the env adn all and creating the tables

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+// Create an Express app
+const app = express();
+
+// Use cors middleware
+app.use(cors());
+
+// Use express json middleware
+app.use(express.json());
+
+// Import migration scripts
+import { createAllTables } from "./db/migrations/createTables.migration.js";
+
+// Create all tables
+const createAllTablesRes = await createAllTables();
+
+// Check if all tables were created
+if (createAllTablesRes.status === 'fail') {
+    // Log the error
+    console.error("APP => Failed to create all tables: ", createAllTablesRes.errors);
+
+    // Exit the application
+    process.exit(1);
+}
+
+export default app;

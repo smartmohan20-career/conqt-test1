646f1f5606bca257b697d9ed8c67dc8d5ad71bdc
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 15:00:02 +0530

    Implemented server file for entry point file

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import app from "./app.js";
+import config from "./config/env.js";
+
+// Retrieve required details from config
+const PORT = config.port;
+
+app.listen(PORT, () => {
+  console.log("Server is running on port 3000");
+});

8a62c551cf56389e4e8e135f254eccc6d32bc7d4
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 15:43:51 +0530

    Added some env variables in config file

--- config/env.js
+++ config/env.js
@@ -5,8 +5,13 @@ DotEnv.config();
 
 // Define the configuration object
 const config = {
-    environment: process.env.ENVIRONMENT,
-    port: process.env.PORT,
+    ENVIRONMENT: process.env.ENVIRONMENT,
+    PORT: process.env.PORT,
+    DB_USER: process.env.DB_USER,
+    DB_HOST: process.env.DB_HOST,
+    DB_NAME: process.env.DB_NAME,
+    DB_PASS: process.env.DB_PASSWORD,
+    DB_PORT: process.env.DB_PORT,
 };
 
 export default config;

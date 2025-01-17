e1e7aa35fe4885542e4f974b1742c350697e47a4
smartmohan20 <dev.smartmohan@gmail.com>
2025-01-17 15:43:32 +0530

    Changes for env variable name

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import config from "./config/env.js";
 
 // Retrieve required details from config
-const PORT = config.port;
+const PORT = config.PORT;
 
 app.listen(PORT, () => {
   console.log("Server is running on port 3000");
